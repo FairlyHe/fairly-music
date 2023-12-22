4f52a61f5af3300fe3599a86956907f530c94ea6
Fairly <hesihai@sina.com>
2023-12-22 15:22:11 +0800

    updata

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,171 @@
-import {createRouter, createWebHistory} from "vue-router"
-// 进度条
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import { isLooseLoggedIn, isAccountLoggedIn } from '@/utils/auth';
 
-import routes from './routes'
+Vue.use(VueRouter);
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/home.vue'),
+    meta: {
+      keepAlive: true,
+      savePosition: true,
+    },
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login.vue'),
+  },
+  {
+    path: '/login/username',
+    name: 'loginUsername',
+    component: () => import('@/views/loginUsername.vue'),
+  },
+  {
+    path: '/login/account',
+    name: 'loginAccount',
+    component: () => import('@/views/loginAccount.vue'),
+  },
+  {
+    path: '/playlist/:id',
+    name: 'playlist',
+    component: () => import('@/views/playlist.vue'),
+  },
+  {
+    path: '/album/:id',
+    name: 'album',
+    component: () => import('@/views/album.vue'),
+  },
+  {
+    path: '/artist/:id',
+    name: 'artist',
+    component: () => import('@/views/artist.vue'),
+    meta: {
+      keepAlive: true,
+      savePosition: true,
+    },
+  },
+  {
+    path: '/artist/:id/mv',
+    name: 'artistMV',
+    component: () => import('@/views/artistMV.vue'),
+    meta: {
+      keepAlive: true,
+    },
+  },
+  {
+    path: '/mv/:id',
+    name: 'mv',
+    component: () => import('@/views/mv.vue'),
+  },
+  {
+    path: '/next',
+    name: 'next',
+    component: () => import('@/views/next.vue'),
+    meta: {
+      keepAlive: true,
+      savePosition: true,
+    },
+  },
+  {
+    path: '/search/:keywords?',
+    name: 'search',
+    component: () => import('@/views/search.vue'),
+    meta: {
+      keepAlive: true,
+    },
+  },
+  {
+    path: '/search/:keywords/:type',
+    name: 'searchType',
+    component: () => import('@/views/searchType.vue'),
+  },
+  {
+    path: '/new-album',
+    name: 'newAlbum',
+    component: () => import('@/views/newAlbum.vue'),
+  },
+  {
+    path: '/explore',
+    name: 'explore',
+    component: () => import('@/views/explore.vue'),
+    meta: {
+      keepAlive: true,
+      savePosition: true,
+    },
+  },
+  {
+    path: '/library',
+    name: 'library',
+    component: () => import('@/views/library.vue'),
+    meta: {
+      requireLogin: true,
+      keepAlive: true,
+      savePosition: true,
+    },
+  },
+  {
+    path: '/library/liked-songs',
+    name: 'likedSongs',
+    component: () => import('@/views/playlist.vue'),
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: () => import('@/views/settings.vue'),
+  },
+  {
+    path: '/daily/songs',
+    name: 'dailySongs',
+    component: () => import('@/views/dailyTracks.vue'),
+    meta: {
+      requireAccountLogin: true,
+    },
+  },
+  {
+    path: '/lastfm/callback',
+    name: 'lastfmCallback',
+    component: () => import('@/views/lastfmCallback.vue'),
+  },
+];
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+const router = new VueRouter({
+  mode: process.env.IS_ELECTRON ? 'hash' : 'history',
+  routes,
+});
+
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
+};
+
 router.beforeEach((to, from, next) => {
-    // 进度条
-    NProgress.start()
-    // 验证当前路由所有的匹配中是否需要有登录验证的
-    if (to.matched.some(r => r.meta.auth)) {
-        // 这里暂时将cookie里是否存有token作为验证是否登录的条件
-        // 请根据自身业务需要修改
-        const token = 12312312
-        if (token && token !== 'undefined') {
-            next()
-        } else {
-            // 没有登录的时候跳转到登录界面
-            // 携带上登陆成功之后需要跳转的页面完整路径
-            next({
-                name: 'login',
-                query: {
-                    redirect: to.fullPath
-                }
-            })
-            // https://github.com/d2-projects/d2-admin/issues/138
-            NProgress.done()
-        }
+  // 需要登录的逻辑
+  if (to.meta.requireAccountLogin) {
+    if (isAccountLoggedIn()) {
+      next();
     } else {
-        // 不需要身份校验 直接通过
-        next()
+      next({ path: '/login/account' });
     }
-})
+  }
+  if (to.meta.requireLogin) {
+    if (isLooseLoggedIn()) {
+      next();
+    } else {
+      if (process.env.IS_ELECTRON === true) {
+        next({ path: '/login/account' });
+      } else {
+        next({ path: '/login' });
+      }
+    }
+  } else {
+    next();
+  }
+});
 
-router.afterEach(to => {
-    // 进度条
-    NProgress.done()
-})
-
-export default router
+export default router;
